0dc638fa19903115cfc09daaa7c2ed52e0626d6f
AES MARKETING LTDA <infra@aesmkt.com.br>
2025-01-06 00:01:11 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#FF6A00',
+        'secondary': '#D65E09',
+        'fade-green': '#00A91F',
+        'fade-terroso': '#1D0806'
+      },
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif'],
+    },
+  },
+  },
+  plugins: [
+
+    ]
+}
+
